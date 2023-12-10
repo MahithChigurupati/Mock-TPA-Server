7834728b503770450a57830cc8df5ea6fd5a2e4d
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-12-10 15:31:31 -0500

    stable build 1

--- api/controller/user-controller.js
+++ api/controller/user-controller.js
@@ -7,6 +7,10 @@ const moment = require("moment")
 const logger = require("../../logger")
 const { exec } = require("child_process")
 
+const ethers = require("ethers")
+const addresses = require("../../constants/contractAddresses.json")
+const contractABI = require("../../constants/abi.json")
+
 const sequelize = require("../model/index")
 
 // importing db models
@@ -79,8 +83,6 @@ const sendSms = async (req, res) => {
     })
 
     if (user == null) {
-        logger.info(`GET: Success`)
-
         res.status(404).send("Not found")
     }
 
@@ -101,8 +103,6 @@ const sendSms = async (req, res) => {
             })
 
             if (otp == null) {
-                logger.info(`GET: Success`)
-
                 let newOTP = {
                     phone: toPhoneNumber,
                     otp: generatedOtp,
@@ -121,7 +121,7 @@ const sendSms = async (req, res) => {
                 )
             }
 
-            res.send(`OTP sent successfully to ${toPhoneNumber}`)
+            res.status(200).send(`OTP sent successfully to ${toPhoneNumber}`)
         })
         .catch((error) => {
             console.log(error)
@@ -136,38 +136,66 @@ const verifyOtp = async (req, res) => {
         return res.status(400).send("Phone number and OTP are required.")
     }
 
-    let otpRecord = await OTP.findOne({
-        where: { phone: phoneNumber },
-    })
+    let otpRecord = await OTP.findOne({ where: { phone: phoneNumber } })
 
-    if (otpRecord == null) {
-        return res.status(404).send("Not found")
+    if (!otpRecord) {
+        return res.status(404).send("OTP record not found.")
     }
 
     if (otpRecord.otp === otp) {
-        // Retrieve the user record
-        let user = await User.findOne({
-            where: { phone: phoneNumber },
-        })
+        console.log("OTP verified")
 
-        if (user) {
-            // Construct the command with user details
-            const command = `cd ../smart-contracts/ && npx hardhat custom-mint --signer ${otpRecord.address} --firstname ${user.first_name} --lastname ${user.last_name} --dob ${user.dob} --phone ${user.phone} --network sepolia`
+        let user = await User.findOne({ where: { phone: phoneNumber } })
 
-            // Execute the command
-            exec(command, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`)
-                    return res.status(500).send("Error executing hardhat command")
-                }
-                console.log(`stdout: ${stdout}`)
-                console.error(`stderr: ${stderr}`)
-
-                res.status(200).send(stdout)
-            })
-        } else {
-            res.status(404).send("User not found")
+        if (!user) {
+            return res.status(404).send("User not found")
         }
+
+        console.log(`Running hardhat command for ${otpRecord.address}`)
+
+        const command = `cd ../smart-contracts/ && npx hardhat custom-mint --signer ${otpRecord.address} --firstname ${user.first_name} --lastname ${user.last_name} --dob ${user.dob} --phone ${user.phone} --network sepolia`
+
+        exec(command, async (error, stdout) => {
+            if (error) {
+                console.error(`Exec error: ${error}`)
+                return res.status(500).send("Error executing hardhat command")
+            }
+
+            console.log(stdout)
+
+            console.log("Command executed, fetching ID data from blockchain")
+
+            const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL)
+            const contractAddress = addresses["11155111"][0]
+
+            if (!contractAddress) {
+                return res.status(500).send("Contract address not found.")
+            }
+
+            const contract = new ethers.Contract(contractAddress, contractABI, provider)
+
+            try {
+                const idData = await contract.getID(otpRecord.address)
+
+                // convert dob to timestamp
+                const dobTimestamp = Date.parse(user.dob)
+
+                const responseObj = {
+                    address: otpRecord.address,
+                    idType: user.id_type,
+                    firstName: user.first_name,
+                    lastName: user.last_name,
+                    dateOfBirth: dobTimestamp,
+                    phone: user.phone,
+                    UID: idData.UID,
+                }
+
+                res.status(200).json(responseObj)
+            } catch (contractError) {
+                console.error(`Contract call error: ${contractError}`)
+                res.status(500).send("Error retrieving ID data from contract.")
+            }
+        })
     } else {
         res.status(401).send("Invalid OTP.")
     }
